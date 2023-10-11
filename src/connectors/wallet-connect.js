13df3aa582a3bb978f5103a540f355a8716f6d88
cheeming <dragon.steal75@gmail.com>
2023-10-12 00:00:59 +0900

    issue #97 Fix an setting for wallet connect

--- src/connectors/wallet-connect.js
+++ src/connectors/wallet-connect.js
@@ -1,13 +1,19 @@
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+import { getConfig } from "../config/config";
+
+const config = getConfig();
 
 const RPC_URLS = {
-  361: "https://eth-rpc-api.thetatoken.org/rpc",
-  2222: "https://evm.kava.io",
+  // 361: "https://eth-rpc-api.thetatoken.org/rpc",
+  // 2222: "https://evm.kava.io",
 };
+
+RPC_URLS[config.thetaChainId] = config.thetaBlockExplorerUrl;
+RPC_URLS[config.kavaChainId] = config.kavaBlockExplorerUrl;
 
 export const walletconnect = new WalletConnectConnector({
   rpc: RPC_URLS,
-  chainId: 361,
-  bridge: "https://bridge.walletconnect.org",
+  chainId: config.thetaChainId, // 361,
+  bridge: config.thetaBlockExplorerUrl, // "https://bridge.walletconnect.org",
   qrcode: true,
 });
