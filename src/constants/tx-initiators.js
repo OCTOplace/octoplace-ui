8109300067e8c9398b582398e8defbdf308383db
Shivam Singh <shivamsingh071093@gmail.com>
2023-12-07 22:41:46 +0530

    implemented for collection comments

--- src/constants/tx-initiators.js
+++ src/constants/tx-initiators.js
@@ -1,6 +1,7 @@
  export const txInitiators = {
     NO_INITIATOR: "no_initiator",
-    POST_NFT_COMMENT: "post_nft_comment"
+    POST_NFT_COMMENT: "post_nft_comment",
+    POST_COLLECTION_COMMENT: "post_collection_comment"
 }
 
 export const txStatus = {
